39a2ab68699d1ce6397bc88f719ce36d829921b5
Тима <TimofeyKolotovkin2@gmail.com>
2025-12-24 21:22:51 +0300

    feat: add dependency injection

--- cmd/todo-service/main.go
+++ cmd/todo-service/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
+
+	"github.com/FooxyS/todo-service/internal/adapter/memory"
+	"github.com/FooxyS/todo-service/internal/controller/rest"
+	"github.com/FooxyS/todo-service/internal/usecase"
 )
 
 func main() {
@@ -10,4 +15,20 @@ func main() {
 	handler := slog.NewTextHandler(os.Stdout, nil)
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
+
+	// DB
+	db := memory.New()
+
+	// usecase
+	uc := usecase.New(db)
+
+	// http handlers and router
+	router := rest.NewRouter(uc)
+
+	slog.Info("start server", "host", "localhost", "port", 8080)
+
+	if err := http.ListenAndServe("localhost:8080", router); err != nil {
+		slog.Error("failed to start server", "error", err)
+		os.Exit(1)
+	}
 }
